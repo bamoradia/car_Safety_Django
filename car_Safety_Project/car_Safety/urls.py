3a5a0a9998fe0a6598a29c40413f96cd92dfc6bf
Badal Moradia <badal@i360llc.com>
2018-08-26 13:55:15 -0500

    made initial api call from browser to test json, and requests packages

--- car_Safety_Project/car_Safety/urls.py
+++ car_Safety_Project/car_Safety/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+	path('api/v1/topten', views.top_ten_list, name='top_ten_list'),
 ]
--- car_Safety_Project/car_Safety/views.py
+++ car_Safety_Project/car_Safety/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Car, Recall, IIHS
+from django.http import JsonResponse, QueryDict
+from django.views.decorators.csrf import csrf_exempt
+import requests
+import json
 
 # Create your views here.
-
+def top_ten_list(request):
+	return JsonResponse({'status': 200, 'data': 'This is the test to get the top_ten_list of cars'})
