161a744b10cd47f60d76668c682b28e149efb487
Badal Moradia <badal@i360llc.com>
2018-08-27 09:37:55 -0500

    get model years sends correct information back to client

--- car_Safety_Project/car_Safety/urls.py
+++ car_Safety_Project/car_Safety/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('api/v1/topten', views.top_ten_list, name='top_ten_list'),
+	path('api/v1/modelyears', views.get_model_years, name='get_model_years'),
 ]
--- car_Safety_Project/car_Safety/views.py
+++ car_Safety_Project/car_Safety/views.py
@@ -2,9 +2,62 @@ from django.shortcuts import render
 from .models import Car, Recall, IIHS
 from django.http import JsonResponse, QueryDict
 from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 import requests
 import json
 
 # Create your views here.
+
+#returns the top ten cars with the highest aggregate score (cars which are already in the database)
 def top_ten_list(request):
 	return JsonResponse({'status': 200, 'data': 'This is the test to get the top_ten_list of cars'})
+
+
+
+# return a list of the years which can be used for the serach
+def get_model_years(request):
+	if request.method == 'GET':
+		fetch_response = requests.get('https://one.nhtsa.gov/webapi/api/SafetyRatings?format=json')
+
+		print(fetch_response)
+
+		response_json = fetch_response.json()
+
+		def response(x): 
+			if x['ModelYear'] < 2100:
+				return x['ModelYear']
+			else: 
+				return 'null' #Trying to return a javascript null
+
+		years = list(map(response, response_json['Results']))
+
+		print(years)
+
+		return JsonResponse({'status': 200, 'data': years})
+	else: 
+		return JsonResponse('status': 400, 'data': 'Cannot Get')
+
+
+def get_makes(request):
+	if request.method == 'POST':
+		return JsonResponse('POSTED')
+	else: 
+		return JsonResponse('status': 400, 'data': 'Cannot Post')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
