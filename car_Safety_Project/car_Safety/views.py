0b8186278010ea24764cde4a9aafe05ae58eb916
Badal Moradia <badal@i360llc.com>
2018-08-27 17:01:26 -0500

    calling all 3 apis and adding information to db if not already exists

--- car_Safety_Project/car_Safety/views.py
+++ car_Safety_Project/car_Safety/views.py
@@ -23,7 +23,7 @@ def top_ten_list(request):
 
 	# after the top_ten list is made, need to find the associated recalls for each of the top ten as well as the IIHS car information and consolidate the information
 
-	
+
 
 	return JsonResponse({'status': 200, 'data': 'This is the test to get the top_ten_list of cars'})
 
@@ -187,40 +187,184 @@ def get_trims(request):
 def get_vehicle_info(request):
 	if request.method == 'POST':
 
-		# make first request to nhtsa to get information about specific model
-		response_nhtsa_info = requests.get('https://one.nhtsa.gov/webapi/api/SafetyRatings/VehicleId/{}?format=json'.format(request.POST['vehicleid']))
+		# check if vehicle exists in the database
+		car_nhtsa = Car.objects.filter(vehicle_id=int(request.POST['vehicleid'])).exists()
 
-		response_nhtsa_info_json = response_nhtsa_info.json()
+		if not car_nhtsa:
+			# this happens if the vehicle is not in the database
+			# make first request to nhtsa to get information about specific model
+			response_nhtsa_info = requests.get('https://one.nhtsa.gov/webapi/api/SafetyRatings/VehicleId/{}?format=json'.format(request.POST['vehicleid']))
 
-		# use response from nhsta to make another request to get recall information about vehicle
-		response_nhtsa_recall = requests.get('https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/{}/make/{}/model/{}?format=json'.format(response_nhtsa_info_json['Results'][0]['ModelYear'], response_nhtsa_info_json['Results'][0]['Make'], response_nhtsa_info_json['Results'][0]['Model']))
+			nhtsa_info_json = response_nhtsa_info.json()
 
-		response_nhtsa_recall_json = response_nhtsa_recall.json()
+			# use response from nhsta to make another request to get recall information about vehicle
+			nhtsa_recall = requests.get('https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/{}/make/{}/model/{}?format=json'.format(nhtsa_info_json['Results'][0]['ModelYear'], nhtsa_info_json['Results'][0]['Make'], nhtsa_info_json['Results'][0]['Model']))
 
-		# use basic information to make request to iihs to find all vehicle series associated with that year and model
-		response_iihs_vehicle_series = requests.get('https://api.iihs.org/V4/ratings/series/{}/{}?apikey=uZBIc3DS8k6evZTw62xttB2dkklf-3ZCqVRpT6CCKP4&format=json'.format(response_nhtsa_info_json['Results'][0]['ModelYear'], response_nhtsa_info_json['Results'][0]['Make'].lower()))
+			nhtsa_recall_json = nhtsa_recall.json()
 
-
-		response_iihs_vehicle_series_json = response_iihs_vehicle_series.json()
-
-		# search the models returned to match model name from nhtsa
-		for i in range(0, len(response_iihs_vehicle_series_json)):
-			index = response_iihs_vehicle_series_json[i]['name'].find(response_nhtsa_info_json['Results'][0]['Model'])
-			if index >= 0:
-				# if match is found, save slug name (which is used to make next request)
-				name_of_vehicle = response_iihs_vehicle_series_json[i]['slug']
+			# use basic information to make request to iihs to find all vehicle series associated with that year and model
+			iihs_vehicle_series = requests.get('https://api.iihs.org/V4/ratings/series/{}/{}?apikey=uZBIc3DS8k6evZTw62xttB2dkklf-3ZCqVRpT6CCKP4&format=json'.format(nhtsa_info_json['Results'][0]['ModelYear'], nhtsa_info_json['Results'][0]['Make'].lower()))
 
 
-		# make request to iihs for full vehicle information based on slug name found above
-		response_iihs_info = requests.get('https://api.iihs.org/V4/ratings/single/{}/{}/{}/?apikey=uZBIc3DS8k6evZTw62xttB2dkklf-3ZCqVRpT6CCKP4&format=json'.format(response_nhtsa_info_json['Results'][0]['ModelYear'], response_nhtsa_info_json['Results'][0]['Make'].lower(), name_of_vehicle))
+			iihs_vehicle_series_json = iihs_vehicle_series.json()
 
-		response_iihs_info_json = response_iihs_info.json()
+			# search the models returned to match model name from nhtsa
+			for i in range(0, len(iihs_vehicle_series_json)):
+				index = iihs_vehicle_series_json[i]['name'].find(nhtsa_info_json['Results'][0]['Model'])
+				if index >= 0:
+					# if match is found, save slug name (which is used to make next request)
+					name_of_vehicle = iihs_vehicle_series_json[i]['slug']
+
+
+			# make request to iihs for full vehicle information based on slug name found above
+			iihs_info = requests.get('https://api.iihs.org/V4/ratings/single/{}/{}/{}/?apikey=uZBIc3DS8k6evZTw62xttB2dkklf-3ZCqVRpT6CCKP4&format=json'.format(nhtsa_info_json['Results'][0]['ModelYear'], nhtsa_info_json['Results'][0]['Make'].lower(), name_of_vehicle))
+
+			iihs_info_json = iihs_info.json()
+
+
+
+			# -------------------SAVING CAR MODEL------------------------- #
+
+			car_instance = Car(
+				model_year= nhtsa_info_json['Results'][0]['ModelYear'],
+				make= nhtsa_info_json['Results'][0]['Make'],
+				model= nhtsa_info_json['Results'][0]['Model'],
+				safety_score= '00',
+				vehicle_description= nhtsa_info_json['Results'][0]['VehicleDescription'],
+				vehicle_id= nhtsa_info_json['Results'][0]['VehicleId'],
+				overall_rating= nhtsa_info_json['Results'][0]['OverallRating'],
+				overall_front_crash_rating= nhtsa_info_json['Results'][0]['OverallFrontCrashRating'],
+				front_crash_driverside_rating= nhtsa_info_json['Results'][0]['FrontCrashDriversideRating'],
+				front_crash_passengerside_rating= nhtsa_info_json['Results'][0]['FrontCrashPassengersideRating'],
+				overall_side_crash_rating= nhtsa_info_json['Results'][0]['OverallSideCrashRating'],
+				side_crash_driverside_rating= nhtsa_info_json['Results'][0]['SideCrashDriversideRating'],
+				side_crash_passengerside_rating= nhtsa_info_json['Results'][0]['SideCrashPassengersideRating'],
+				rollover_rating= nhtsa_info_json['Results'][0]['RolloverRating'],
+				side_pole_crash_rating= nhtsa_info_json['Results'][0]['SidePoleCrashRating']
+			)
+
+			car_instance.save()
+
+			# -------------------SAVING CAR MODEL------------------------- #
+			# -------------------SAVING RECALL MODEL---------------------- #
+
+			all_recalls = []
+
+			if len(nhtsa_recall_json['Results']) == 0:
+				recall = Recall(
+					car= car_instance,
+					manufacturer= 'NA',
+					component= 'NA',
+					consequence= 'NA',
+					summary= 'NA',
+					remedy= 'NA',
+					notes= 'NA',
+					model_year= 'NA',
+					make= 'NA',
+					model= 'NA',
+					report_received_date= 'NA',
+					nhtsa_campaign_number= 'NA'
+				)
+				recall.save()
+
+				all_recalls.append(recall)
+
+			else:
+				for i in range(0, len(nhtsa_recall_json['Results'])):
+
+					recall = Recall(
+						car= car_instance,
+						manufacturer= nhtsa_recall_json['Results'][i]['Manufacturer'],
+						component= nhtsa_recall_json['Results'][i]['Component'],
+						consequence= nhtsa_recall_json['Results'][i]['Consequence'],
+						summary= nhtsa_recall_json['Results'][i]['Summary'],
+						remedy= nhtsa_recall_json['Results'][i]['Remedy'],
+						notes= nhtsa_recall_json['Results'][i]['Notes'],
+						model_year= nhtsa_recall_json['Results'][i]['ModelYear'],
+						make= nhtsa_recall_json['Results'][i]['Make'],
+						model= nhtsa_recall_json['Results'][i]['Model'],
+						report_received_date= nhtsa_recall_json['Results'][i]['ReportReceivedDate'],
+						nhtsa_campaign_number= nhtsa_recall_json['Results'][i]['NHTSACampaignNumber']
+					)
+					recall.save()
+
+					all_recalls.append(recall)
+
+
+			# -------------------SAVING RECALL MODEL---------------------- #
+			# -------------------SAVING IIHS MODEL------------------------ #
+
+			iihs = IIHS(
+					car= car_instance,
+					iihs_id= iihs_info_json[0]['id'],
+					vehicle_description= nhtsa_info_json['Results'][0]['VehicleDescription'],
+					model_year= iihs_info_json[0]['modelYear'],
+					make= iihs_info_json[0]['make']['name'],
+					model= iihs_info_json[0]['name'],
+					class_name= iihs_info_json[0]['class']['name'],
+					top_safety_pick= iihs_info_json[0]['topSafetyPick']['isTopSafetyPickPlus'],
+					tsp_year= iihs_info_json[0]['topSafetyPick']['tspYear'],
+					tsp_is_qualified= iihs_info_json[0]['topSafetyPick']['isQualified'],
+					tsp_built_after= iihs_info_json[0]['topSafetyPick']['builtAfter'],
+					tsp_qualifying_text= iihs_info_json[0]['topSafetyPick']['qualifyingText'],
+					frmo_qualifying_text= iihs_info_json[0]['frontalRatingsModerateOverlap'][0]['qualifyingText'],
+					frmo_built_before= iihs_info_json[0]['frontalRatingsModerateOverlap'][0]['builtBefore'],
+					frmo_built_after= iihs_info_json[0]['frontalRatingsModerateOverlap'][0]['builtAfter'],
+					frmo_overall_rating= iihs_info_json[0]['frontalRatingsModerateOverlap'][0]['overallRating'],
+					frso_qualifying_text= iihs_info_json[0]['frontalRatingsSmallOverlap'][0]['qualifyingText'],
+					frso_built_before= iihs_info_json[0]['frontalRatingsSmallOverlap'][0]['builtBefore'],
+					frso_built_after= iihs_info_json[0]['frontalRatingsSmallOverlap'][0]['builtAfter'],
+					frso_overall_rating= iihs_info_json[0]['frontalRatingsSmallOverlap'][0]['overallRating'],
+					frsop_qualifying_text= iihs_info_json[0]['frontalRatingsSmallOverlapPassenger'][0]['qualifyingText'],
+					frsop_built_before= iihs_info_json[0]['frontalRatingsSmallOverlapPassenger'][0]['builtBefore'],
+					frsop_built_after= iihs_info_json[0]['frontalRatingsSmallOverlapPassenger'][0]['builtAfter'],
+					frsop_overall_rating= iihs_info_json[0]['frontalRatingsSmallOverlapPassenger'][0]['overallRating'],
+					sr_qualifying_text= iihs_info_json[0]['sideRatings'][0]['qualifyingText'],
+					sr_built_before= iihs_info_json[0]['sideRatings'][0]['builtBefore'],
+					sr_built_after= iihs_info_json[0]['sideRatings'][0]['builtAfter'],
+					sr_overall_rating= iihs_info_json[0]['sideRatings'][0]['overallRating'], 
+					rollover_qualifying_text= iihs_info_json[0]['rolloverRatings'][0]['qualifyingText'],
+					rollover_built_before= iihs_info_json[0]['rolloverRatings'][0]['builtBefore'],
+					rollover_built_after= iihs_info_json[0]['rolloverRatings'][0]['builtAfter'],
+					rollover_overall_rating= iihs_info_json[0]['rolloverRatings'][0]['overallRating'],
+					rear_qualifying_text= iihs_info_json[0]['rearRatings'][0]['qualifyingText'],
+					rear_built_before= iihs_info_json[0]['rearRatings'][0]['builtBefore'], 
+					rear_built_after= iihs_info_json[0]['rearRatings'][0]['builtAfter'],
+					rear_overall_rating= iihs_info_json[0]['rearRatings'][0]['overallRating'],
+					fcpr_qualifying_text= iihs_info_json[0]['frontCrashPreventionRatings'][0]['qualifyingText'],
+					fcpr_built_before= iihs_info_json[0]['frontCrashPreventionRatings'][0]['builtBefore'],
+					fcpr_built_after= iihs_info_json[0]['frontCrashPreventionRatings'][0]['builtAfter'],
+					fcpr_total_points= iihs_info_json[0]['frontCrashPreventionRatings'][0]['overallRating']['totalPoints'],
+					fcpr_rating_text= iihs_info_json[0]['frontCrashPreventionRatings'][0]['overallRating']['ratingText']
+				)
+
+			iihs.save()
+
+			# -------------------SAVING IIHS MODEL------------------------ #
+
+
+			return JsonResponse({'status': 200, 'data': {'nhtsa': [nhtsa_info_json['Results'][0]], 'recall': nhtsa_recall_json['Results'], 'iihs': [iihs_info_json[0]]}})
+
+
+		else: 
+			car_nhtsa = Car.objects.get(vehicle_id=int(request.POST['vehicleid']))
+			# vehicle was found in the database
+			iihs = IIHS.objects.get(car=car_nhtsa)
+			recall = Recall.objects.filter(car=car_nhtsa)
 
 
 
 
-		return JsonResponse({'status': 200, 'data': {'nhtsa': response_nhtsa_info_json['Results'][0], 'recall': response_nhtsa_recall_json['Results'], 'iihs': response_iihs_info_json[0]}})
+			car_serialized1 = serializers.serialize('json', [car_nhtsa])
+			iihs_serialized1 = serializers.serialize('json', [iihs])
+			recall_serialized1 = serializers.serialize('json', recall)
 
+			car_serialized = json.loads(car_serialized1)
+			iihs_serialized = json.loads(iihs_serialized1)
+			recall_serialized = json.loads(recall_serialized1)
+
+
+			return JsonResponse({'status': 200, 'data': {'nhtsa': car_serialized, 'recall': recall_serialized, 'iihs': iihs_serialized}})
 	else:
 		return JsonResponse({'status': 400, 'data': 'Cannot POST to /vehicleinfo'}) 
 
